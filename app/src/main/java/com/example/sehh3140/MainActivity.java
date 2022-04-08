4a1e951e6cc56cc771a19f00d99331ba854ff6dc
vireitD <likawa25135@gmail.com>
2022-04-08 11:31:20 +0800

    new 1

--- app/src/main/java/com/example/sehh3140/MainActivity.java
+++ app/src/main/java/com/example/sehh3140/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.sehh3140;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et1,et2 ;
+    Button register,login,guest;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et1 =findViewById(R.id.et1);
+        et2 =findViewById(R.id.et2);
+        register = findViewById(R.id.register);
+        login = findViewById(R.id.login);
+        guest = findViewById(R.id.Guest);
     }
 }
