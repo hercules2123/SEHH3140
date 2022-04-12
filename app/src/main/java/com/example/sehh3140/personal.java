06e7fd37f7fffdb36ed8adc42f184747440a6856
vireitD <likawa25135@gmail.com>
2022-04-12 19:30:53 +0800

    new 1

--- app/src/main/java/com/example/sehh3140/personal.java
+++ app/src/main/java/com/example/sehh3140/personal.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,8 +19,10 @@ public class personal extends Fragment {
     private String mParam1;
     private String mParam2;
     userinfo userinfo =new userinfo();
+    changepassword changepassword = new changepassword();
+    couponview couponview = new couponview();
     String[] a = {
-            "","",""
+            "個人資料","修改密碼","查看優惠券"
     };
     ListView lv1 ;
 
@@ -60,6 +63,26 @@ public class personal extends Fragment {
        View view = inflater.inflate(R.layout.fragment_personal, container, false);
 
         lv1 = view.findViewById(R.id.lv1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), R.layout.listview,R.id.tv,a);
+        lv1.setAdapter(adapter);
+        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position==0){
+                    FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                    transaction.replace(R.id.Fragment, userinfo);
+                    transaction.commit();
+                } else if(position==1){
+                    FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                    transaction.replace(R.id.Fragment, changepassword);
+                    transaction.commit();
+                }else if(position==2){
+                    FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                    transaction.replace(R.id.Fragment, couponview);
+                    transaction.commit();
+                }
+            }
+        });
 
         return view;
     }
